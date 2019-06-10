c00b1ae0c79d1001094addd0c309067c2be5edf9
Ogunboyejo Olatunde <tunde.ogunboyejo@xputertechnologies.com>
2019-06-10 18:21:16 +0100

    created the server and set up file

---
+++ routes/api/user.js
@@ -0,0 +1,3 @@
+module.exports = app => {
+  app.get('/api/users', (req, res, next) => {});
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Yo men');
+});
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+  mongoose.set('useFindAndModify', false);
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  })
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => console.log(err));
+
+db.once('open', () => {
+  require('./routes/api/indexRoutes')(app);
+  require('./routes/api/user')(app);
+  require('./routes/api/profile')(app);
+  console.log(`Server running on port ${port}`);
+});
